cc1b4146c8f40faff6dc54c2d83f75ea06d1133b
metaras <metaras.c@ku.th>
2021-03-19 23:04:13 +0700

    fix add product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
 
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_admin!, except: [:index, :show]
   def index
     @search = params[:search]
     @products = Product.all
